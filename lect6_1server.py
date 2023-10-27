9cdececaaaa9178fa68d96ee1cdaa6ee4fe994e2
hyunnnnj <124047728+hyunnnnj@users.noreply.github.com>
2023-10-27 22:42:02 +0900

    1027

--- lect6_1server.py
+++ lect6_1server.py
@@ -82,3 +82,103 @@ while True:
     sock.sendto(rsp.encode("utf-8"), addr)
 
 sock.close() """
+
+#==========================================================
+#7-2
+
+#클래스 & 메서드
+""" class Person :
+	name = "python"
+	age = 23
+	number = "01012345678" """
+ 
+""" class Person :
+    name = "python"
+    age = 23
+    number = "01012345678"
+
+    def getIntroduce(self):
+        return f"My name is {self.name}" 
+        
+p=Person()
+print(p.name)
+print(p.age)
+print(p.number)
+print(p.getIntroduce())
+
+p1=Person()
+print(p1.name)
+print(p1.age)
+print(p1.number)
+print(p1.getIntroduce())
+"""
+
+
+#생성자
+""" class Person :
+	def __init__(self, name, age, number):
+		self.name = name
+		self.age = age
+		self.number = number
+  
+
+p = Person("lee",23,1)
+p1 = Person("Park",12,2)
+p2 = Person("Kim",25,3)
+
+print(p.name)
+print(p1.name)
+print(p2.name) """
+
+
+#클래스 변수
+""" class Person :
+	count = 0
+	
+	def __init__(self, name, age,number):
+		self.name = name
+		self.age = age
+		self.number = number
+		Person.count +=1
+  
+
+p = Person("lee",23,1)
+print(p.name)
+print(p.count)
+p1 = Person("Park",12,2)
+print(p1.name)
+print(p1.count)
+p2 = Person("Kim",25,3)
+print(p2.name)
+print(p2.count)
+
+print(p.count)
+print(p1.count)
+print(p2.count) """
+
+
+#클래스 메서드
+class Person :
+	count = 0
+
+	def __init__(self, name, age, number):
+		self.name = name
+		self.age = age
+		self.nmuber = number
+		Person.count += 1
+
+	@classmethod
+	def getCount(cls) : 
+		return cls.count
+
+p = Person("lee",23,1)
+print(p.name)
+print(p.getCount())
+
+p1 = Person("Park",21,2)
+print(p1.name)
+print(p1.getCount())
+
+p2 = Person("Ju",13,3)
+print(p2.name)
+print(p2.getCount())
